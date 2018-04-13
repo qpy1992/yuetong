a8a5fd7a593b0ba9742bf508020365cfbe16f00d
admin <admin>
2018-04-13 08:59:56 +0800

    me first commit

--- app/src/main/java/com/example/win7/ytdemo/YApplication.java
+++ app/src/main/java/com/example/win7/ytdemo/YApplication.java
@@ -3,15 +3,27 @@ package com.example.win7.ytdemo;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.win7.ytdemo.activity.ChatActivity;
+import com.example.win7.ytdemo.activity.MainActivity;
+import com.example.win7.ytdemo.adapter.MessageListenerAdapter;
 import com.example.win7.ytdemo.util.Consts;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMOptions;
+import com.hyphenate.chat.EMTextMessageBody;
 
+import org.greenrobot.eventbus.EventBus;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
@@ -27,12 +39,20 @@ import java.util.List;
 
 public class YApplication extends Application {
     public static ArrayList<Activity> listActivity = new ArrayList<Activity>();
-    public static String fname = "";
-    public static String fgroup = "";
+    public static String              fname        = "";
+    public static String              fgroup       = "";
+    private SoundPool mSoundPool;
+    private int       mDuanSound;
+    private int       mYuluSound;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initHuanxin();
+        initSoundPool();
+    }
+
+    private void initHuanxin() {
         EMOptions options = new EMOptions();
         // 默认添加好友时，是不需要验证的，改成需要验证
         options.setAcceptInvitationAlways(false);
@@ -40,9 +60,46 @@ public class YApplication extends Application {
         EMClient.getInstance().init(getApplicationContext(), options);
         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(true);
+        //添加消息的监听
+        initMessageListener();
+        //监听连接状态的改变
+        //        initConnectionListener();
     }
 
-    public static void exit(){
+    private void initSoundPool() {
+        mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+        mDuanSound = mSoundPool.load(this, R.raw.yulu, 1);
+        mYuluSound = mSoundPool.load(this, R.raw.yulu, 1);
+    }
+
+    private void initMessageListener() {
+        EMClient.getInstance().chatManager().addMessageListener(new MessageListenerAdapter() {
+            @Override
+            public void onMessageReceived(List<EMMessage> list) {
+                super.onMessageReceived(list);
+                if (list != null && list.size() > 0) {
+                    /**
+                     * 1. 判断当前应用是否在后台运行
+                     * 2. 如果是在后台运行，则发出通知栏
+                     * 3. 如果是在后台发出长声音
+                     * 4. 如果在前台发出短声音
+                     */
+                    if (isRuninBackground()) {
+                        sendNotification(list.get(0));
+                        //发出长声音
+                        //参数2/3：左右喇叭声音的大小
+                        mSoundPool.play(mYuluSound, 1, 1, 0, 0, 1);
+                    } else {
+                        //发出短声音
+                        mSoundPool.play(mDuanSound, 1, 1, 0, 0, 1);
+                    }
+                    EventBus.getDefault().post(list.get(0));
+                }
+            }
+        });
+    }
+
+    public static void exit() {
         try {
             for (Activity activity : listActivity) {
                 EMClient.getInstance().logout(true);
@@ -76,7 +133,7 @@ public class YApplication extends Application {
         return processName;
     }
 
-    static class ETask extends AsyncTask<Void,String,String>{
+    static class ETask extends AsyncTask<Void, String, String> {
 
         @Override
         protected String doInBackground(Void... voids) {
@@ -118,7 +175,7 @@ public class YApplication extends Application {
 
             // 获取返回的结果
             if (null != object) {
-                Log.i("返回结果", object.getProperty(0).toString()+"=========================");
+                Log.i("返回结果", object.getProperty(0).toString() + "=========================");
                 String result = object.getProperty(0).toString();
                 if (result.equals("成功")) {
                     return "1";
@@ -128,5 +185,43 @@ public class YApplication extends Application {
             }
             return "3";
         }
+    }
+
+    private boolean isRuninBackground() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> runningTasks = activityManager.getRunningTasks(100);
+        ActivityManager.RunningTaskInfo runningTaskInfo = runningTasks.get(0);
+        if (runningTaskInfo.topActivity.getPackageName().equals(getPackageName())) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void sendNotification(EMMessage message) {
+        EMTextMessageBody messageBody = (EMTextMessageBody) message.getBody();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        //延时意图
+        /**
+         * 参数2：请求码 大于1
+         */
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent chatIntent = new Intent(this, ChatActivity.class);
+        chatIntent.putExtra("username", message.getFrom());
+
+        Intent[] intents = {mainIntent, chatIntent};
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 1, intents, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setAutoCancel(true) //当点击后自动删除
+                .setSmallIcon(R.mipmap.message) //必须设置
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentTitle("您有一条新消息")
+                .setContentText(messageBody.getMessage())
+                .setContentInfo(message.getFrom())
+                .setContentIntent(pendingIntent)
+                .setPriority(Notification.PRIORITY_MAX)
+                .build();
+        notificationManager.notify(1, notification);
     }
 }
--- app/src/main/java/com/example/win7/ytdemo/activity/ChatActivity.java
+++ app/src/main/java/com/example/win7/ytdemo/activity/ChatActivity.java
@@ -1,67 +1,80 @@
 package com.example.win7.ytdemo.activity;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+
 import com.example.win7.ytdemo.R;
-import com.example.win7.ytdemo.adapter.ChatAdapter;
-import com.example.win7.ytdemo.entity.Chat;
+import com.example.win7.ytdemo.adapter.MyChatAdapter;
 import com.example.win7.ytdemo.entity.Item;
-import com.example.win7.ytdemo.util.Utils;
-import com.hyphenate.EMMessageListener;
+import com.example.win7.ytdemo.listener.CallBackListener;
+import com.example.win7.ytdemo.util.ToastUtils;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
 import com.hyphenate.chat.EMMessage;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class ChatActivity extends BaseActivity {
-    EditText et_msg;
-    TextView tv_send;
-    String nickname;
-    RecyclerView rcv_msg;
-    ChatAdapter adapter;
+public class ChatActivity extends BaseActivity implements View.OnClickListener {
+    EditText        et_msg;
+//    TextView        tv_send;
+    Button          bt_send;
+    String          nickname;
+    RecyclerView    rcv_msg;
+//    ChatAdapter     adapter;
     ArrayList<Item> list;
-    String content;
-    Toolbar toolbar;
-    LinearLayout ll_chat,ll_bottom;
+    String          content;
+    Toolbar         toolbar;
+    LinearLayout    ll_chat,ll_bottom;
     public static final int SHOW_RESPONSE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        EMClient.getInstance().chatManager().addMessageListener(msgListener);
+//        EMClient.getInstance().chatManager().addMessageListener(msgListener);
+        initView();
         setTool();
         setViews();
         setListeners();
     }
 
+    private void initView() {
+        toolbar = (Toolbar)findViewById(R.id.id_toolbar);
+        ll_chat = (LinearLayout)findViewById(R.id.ll_chat);
+        ll_bottom = (LinearLayout)findViewById(R.id.ll_bottom);
+        et_msg = (EditText)findViewById(R.id.et_msg);
+        //        tv_send = (TextView)findViewById(R.id.tv_send);
+        bt_send = (Button)findViewById(R.id.bt_send);
+        rcv_msg = (RecyclerView)findViewById(R.id.rcv_msg);
+    }
+
     protected void setTool(){
         nickname = getIntent().getStringExtra("nickname");
-        toolbar = (Toolbar)findViewById(R.id.id_toolbar);
+        if (TextUtils.isEmpty(nickname)){
+            ToastUtils.showToast(this,"stub,请携带username参数！");
+            finish();
+            return;
+        }
         toolbar.setTitle(nickname);
-
         toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
         setSupportActionBar(toolbar);
-
-
         toolbar.setNavigationIcon(android.R.drawable.ic_menu_revert);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,21 +82,50 @@ public class ChatActivity extends BaseActivity {
                 finish();
             }
         });
+        //根据name获取聊天数据
+        getTalkData();
     }
 
+    private void getTalkData() {
+        EMConversation conversation = EMClient.getInstance().chatManager().getConversation(nickname);
+        if (conversation != null) {
+            //需要将所有的未读消息标记为已读
+            conversation.markAllMessagesAsRead();
+            //
+            //曾经有聊天过,那么获取最多最近的20条聊天记录，然后展示到View层
+            //获取最后一条消息
+            EMMessage lastMessage = conversation.getLastMessage();
+            //获取最后一条消息之前的19条（最多）
+            int count = 19;
+            if (mEMMessageList.size()>=19){
+                count = mEMMessageList.size();
+            }
+            List<EMMessage> messageList = conversation.loadMoreMsgFromDB(lastMessage.getMsgId(), count);
+            Collections.reverse(messageList);
+            mEMMessageList.clear();
+            mEMMessageList.add(lastMessage);
+            mEMMessageList.addAll(messageList);
+            Collections.reverse(mEMMessageList);
+        } else {
+            mEMMessageList.clear();
+        }
+    }
+    private List<EMMessage> mEMMessageList = new ArrayList<>();
+    private MyChatAdapter mChatAdapter;
     protected void setViews(){
-        ll_chat = (LinearLayout)findViewById(R.id.ll_chat);
-        ll_bottom = (LinearLayout)findViewById(R.id.ll_bottom);
-        et_msg = (EditText)findViewById(R.id.et_msg);
-        tv_send = (TextView)findViewById(R.id.tv_send);
-        rcv_msg = (RecyclerView)findViewById(R.id.rcv_msg);
-        list = new ArrayList<>();
-        adapter = new ChatAdapter();
-        rcv_msg.setHasFixedSize(true);
-        rcv_msg.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        rcv_msg.setAdapter(adapter);
-//        adapter.replaceAll(list);
-        Utils.autoScrollView(ll_chat, ll_bottom);//弹出软键盘时滚动视图
+//        list = new ArrayList<>();
+//        adapter = new ChatAdapter();
+//        rcv_msg.setHasFixedSize(true);
+//        rcv_msg.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+//        rcv_msg.setAdapter(adapter);
+////        adapter.replaceAll(list);
+//        Utils.autoScrollView(ll_chat, ll_bottom);//弹出软键盘时滚动视图
+        rcv_msg.setLayoutManager(new LinearLayoutManager(this));
+        mChatAdapter = new MyChatAdapter(mEMMessageList);
+        rcv_msg.setAdapter(mChatAdapter);
+        if (mEMMessageList.size()!=0){
+            rcv_msg.scrollToPosition(mEMMessageList.size()-1);
+        }
     }
 
     protected void setListeners(){
@@ -104,83 +146,111 @@ public class ChatActivity extends BaseActivity {
 //            }
 //        });
 
-        tv_send.setOnClickListener(new View.OnClickListener() {
-            @TargetApi(Build.VERSION_CODES.M)
+//        tv_send.setOnClickListener(new View.OnClickListener() {
+//            @TargetApi(Build.VERSION_CODES.M)
+//            @Override
+//            public void onClick(View view) {
+//                content = et_msg.getText().toString();
+//                if(content.equals("")){
+//                    Toast.makeText(ChatActivity.this,"不能发送空消息",Toast.LENGTH_SHORT).show();
+//                }else {
+//                    ArrayList<Item> data = new ArrayList<>();
+//                    //创建一条文本消息，content为消息文字内容，toChatUsername为对方用户或者群聊的id，后文皆是如此
+//                    EMMessage message = EMMessage.createTxtSendMessage(content, nickname);
+//                    //发送消息
+//                    EMClient.getInstance().chatManager().sendMessage(message);
+//                    //将消息放到右边
+//                    Chat chat = new Chat();
+//                    chat.setIcon("http://wx.qlogo.cn/mmopen/V1RSN8IUDAyVjF42G3G1lTBAEiaiam5hmhmnm0MmFRmf61oicTJhVTRAOS9tqjhMc5hmRcGNFQR2VACicSic6xq0m34lpPAKIcOdJ/0");
+//                    chat.setContent(content);
+//                    data.add(new Item(Item.CHAT_B,chat));
+//                    adapter.addAll(data);
+//                    et_msg.setText("");
+//                    hideKeyBorad(et_msg);
+//                }
+//            }
+//        });
+        et_msg.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                content = et_msg.getText().toString();
-                if(content.equals("")){
-                    Toast.makeText(ChatActivity.this,"不能发送空消息",Toast.LENGTH_SHORT).show();
-                }else {
-                    ArrayList<Item> data = new ArrayList<>();
-                    //创建一条文本消息，content为消息文字内容，toChatUsername为对方用户或者群聊的id，后文皆是如此
-                    EMMessage message = EMMessage.createTxtSendMessage(content, nickname);
-                    //发送消息
-                    EMClient.getInstance().chatManager().sendMessage(message);
-                    //将消息放到右边
-                    Chat chat = new Chat();
-                    chat.setIcon("http://wx.qlogo.cn/mmopen/V1RSN8IUDAyVjF42G3G1lTBAEiaiam5hmhmnm0MmFRmf61oicTJhVTRAOS9tqjhMc5hmRcGNFQR2VACicSic6xq0m34lpPAKIcOdJ/0");
-                    chat.setContent(content);
-                    data.add(new Item(Item.CHAT_B,chat));
-                    adapter.addAll(data);
-                    et_msg.setText("");
-                    hideKeyBorad(et_msg);
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().length()==0){
+                    bt_send.setEnabled(false);
+                }else{
+                    bt_send.setEnabled(true);
                 }
             }
         });
+        bt_send.setOnClickListener(this);
+        String msg = et_msg.getText().toString();
+        if (TextUtils.isEmpty(msg)){
+            bt_send.setEnabled(false);
+        }else {
+            bt_send.setEnabled(true);
+        }
+//        EventBus.getDefault().register(this);
     }
 
-    EMMessageListener msgListener = new EMMessageListener() {
-
-        @Override
-        public void onMessageReceived(final List<EMMessage> messages) {
-            //收到消息
-            //将消息放到左边
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    ArrayList<Item> data1 = new ArrayList<>();
-                    Chat chat = new Chat();
-                    chat.setIcon("http://wx.qlogo.cn/mmopen/V1RSN8IUDAyVjF42G3G1lTBAEiaiam5hmhmnm0MmFRmf61oicTJhVTRAOS9tqjhMc5hmRcGNFQR2VACicSic6xq0m34lpPAKIcOdJ/0");
-                    chat.setContent(messages.get(0).getBody().toString().replace("txt:\"","").replace("\"",""));
-                    data1.add(new Item(Item.CHAT_A,chat));
-                    adapter.addAll(data1);
-                }
-            });
-            hideKeyBorad(et_msg);
-            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone rt = RingtoneManager.getRingtone(getApplicationContext(), uri);
-            rt.play();
-        }
-
-        @Override
-        public void onCmdMessageReceived(List<EMMessage> messages) {
-            //收到透传消息
-
-        }
-
-        @Override
-        public void onMessageRead(List<EMMessage> messages) {
-            //收到已读回执
-
-        }
-
-        @Override
-        public void onMessageDelivered(List<EMMessage> message) {
-            //收到已送达回执
-
-        }
-        @Override
-        public void onMessageRecalled(List<EMMessage> messages) {
-            //消息被撤回
-
-        }
-        @Override
-        public void onMessageChanged(EMMessage message, Object change) {
-            //消息状态变动
-
-        }
-    };
+//    EMMessageListener msgListener = new EMMessageListener() {
+//
+//        @Override
+//        public void onMessageReceived(final List<EMMessage> messages) {
+//            //收到消息
+//            //将消息放到左边
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    ArrayList<Item> data1 = new ArrayList<>();
+//                    Chat chat = new Chat();
+//                    chat.setIcon("http://wx.qlogo.cn/mmopen/V1RSN8IUDAyVjF42G3G1lTBAEiaiam5hmhmnm0MmFRmf61oicTJhVTRAOS9tqjhMc5hmRcGNFQR2VACicSic6xq0m34lpPAKIcOdJ/0");
+//                    chat.setContent(messages.get(0).getBody().toString().replace("txt:\"","").replace("\"",""));
+//                    data1.add(new Item(Item.CHAT_A,chat));
+//                    adapter.addAll(data1);
+//                }
+//            });
+//            hideKeyBorad(et_msg);
+//            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//            Ringtone rt = RingtoneManager.getRingtone(getApplicationContext(), uri);
+//            rt.play();
+//        }
+//
+//        @Override
+//        public void onCmdMessageReceived(List<EMMessage> messages) {
+//            //收到透传消息
+//
+//        }
+//
+//        @Override
+//        public void onMessageRead(List<EMMessage> messages) {
+//            //收到已读回执
+//
+//        }
+//
+//        @Override
+//        public void onMessageDelivered(List<EMMessage> message) {
+//            //收到已送达回执
+//
+//        }
+//        @Override
+//        public void onMessageRecalled(List<EMMessage> messages) {
+//            //消息被撤回
+//
+//        }
+//        @Override
+//        public void onMessageChanged(EMMessage message, Object change) {
+//            //消息状态变动
+//
+//        }
+//    };
 
 
     private void hideKeyBorad(View v) {
@@ -193,7 +263,56 @@ public class ChatActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         //记得在不需要的时候移除listener，如在activity的onDestroy()时
-        EMClient.getInstance().chatManager().removeMessageListener(msgListener);
+//        EMClient.getInstance().chatManager().removeMessageListener(msgListener);
         super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt_send:
+                String msg = et_msg.getText().toString();
+                EMMessage emMessage = EMMessage.createTxtSendMessage(msg,nickname);
+                emMessage.setStatus(EMMessage.Status.INPROGRESS);
+                mEMMessageList.add(emMessage);
+                onUpdata(mEMMessageList.size());
+                emMessage.setMessageStatusCallback(new CallBackListener() {
+                    @Override
+                    public void onMainSuccess() {
+                        onUpdata(mEMMessageList.size());
+                    }
+
+                    @Override
+                    public void onMainError(int i, String s) {
+                        onUpdata(mEMMessageList.size());
+                    }
+                });
+                EMClient.getInstance().chatManager().sendMessage(emMessage);
+                et_msg.getText().clear();
+                break;
+        }
+    }
+
+    private void onUpdata(int size) {
+        mChatAdapter.notifyDataSetChanged();
+        if (size!=0){
+            rcv_msg.smoothScrollToPosition(size-1);
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(EMMessage message){
+        //当收到信消息的时候
+        /*
+         *  判断当前这个消息是不是正在聊天的用户给我发的
+         *  如果是，让ChatPresenter 更新数据
+         *
+         */
+        String from = message.getFrom();
+        if (from.equals(nickname)){
+            getTalkData();
+            onUpdata(mEMMessageList.size());
+        }
     }
 }
