99a0e819aa751bed1c4fc07c1c91a466fa08f75c
admin <admin>
2018-06-25 15:34:32 +0800

    login change

--- app/src/main/java/com/example/win7/ytdemo/activity/JiankongActivity.java
+++ app/src/main/java/com/example/win7/ytdemo/activity/JiankongActivity.java
@@ -1,24 +1,34 @@
 package com.example.win7.ytdemo.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.win7.ytdemo.R;
 import com.example.win7.ytdemo.adapter.CameraIdSpAdapter;
+import com.example.win7.ytdemo.util.Consts;
+import com.example.win7.ytdemo.util.ProgressDialogUtil;
+import com.example.win7.ytdemo.util.SoapUtil;
+import com.example.win7.ytdemo.util.ToastUtils;
 import com.ezvizuikit.open.EZUIError;
 import com.ezvizuikit.open.EZUIKit;
 import com.ezvizuikit.open.EZUIPlayer;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class JiankongActivity extends BaseActivity {
     private Toolbar    toolbar;
@@ -26,6 +36,7 @@ public class JiankongActivity extends BaseActivity {
     private Spinner    mSp_camid;
     private String cameraID = "107910292";
     private List<String> mIdData;//存放摄像头ID
+    private String token = "at.9bnl7xjt8rpqsd9t0iwgldl48ijivya9-73rejh53j4-0vp0wbc-cen7sk5pi";
 
     @Override
 
@@ -36,7 +47,7 @@ public class JiankongActivity extends BaseActivity {
         setTool();
         setViews();
         setData();
-        setListeners();
+
     }
 
     protected void setTool() {
@@ -57,6 +68,15 @@ public class JiankongActivity extends BaseActivity {
         mSp_camid = (Spinner) findViewById(R.id.sp_camid);
         //获取EZUIPlayer实例
         mPlayer = (EZUIPlayer) findViewById(R.id.player_ui);
+
+        //获取应用在萤石云上的token
+        getTokenFromYSY();
+    }
+
+    private void getTokenFromYSY() {
+        String getTokenUrl = "select token from z_token where id=1";
+        ItemTask itemTask = new ItemTask(getTokenUrl);
+        itemTask.execute();
     }
 
     private void setData() {
@@ -66,10 +86,13 @@ public class JiankongActivity extends BaseActivity {
 
         CameraIdSpAdapter spAdapter = new CameraIdSpAdapter(JiankongActivity.this, mIdData);
         mSp_camid.setAdapter(spAdapter);
+    }
+
+    private void initCamera() {
         //初始化EZUIKit
         EZUIKit.initWithAppKey(getApplication(), "d55fc8eefc784b9db9acbacd1bab29a2");
         //设置授权token
-        EZUIKit.setAccessToken("at.7kilm6c999h0sv1t8d1vp8207h1xqwr5-13j80h6pbi-0qqvku7-ldhf8od2x");
+        EZUIKit.setAccessToken(token);
         //设置播放回调callback
         mPlayer.setCallBack(new EZUIPlayer.EZUIPlayerCallBack() {
             @Override
@@ -91,7 +114,7 @@ public class JiankongActivity extends BaseActivity {
             @Override
             public void onPrepared() {
                 //回放文件列表
-                //                List<EZRecordFile> playList = mPlayer.getPlayList();
+                //List<EZRecordFile> playList = mPlayer.getPlayList();
                 mPlayer.startPlay();
             }
 
@@ -110,12 +133,13 @@ public class JiankongActivity extends BaseActivity {
 
         //加载中显示的UI
         //创建loadingview
-        ProgressBar mLoadView = new ProgressBar(JiankongActivity.this);
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
-        mLoadView.setLayoutParams(lp);
-        //设置loadingview
-        mPlayer.setLoadingView(mLoadView);
+//        ProgressBar mLoadView = new ProgressBar(JiankongActivity.this);
+//        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+//        mLoadView.setLayoutParams(lp);
+//        //设置loadingview
+//        mPlayer.setLoadingView(mLoadView);
+        setListeners();
     }
 
     protected void setListeners() {
@@ -156,4 +180,55 @@ public class JiankongActivity extends BaseActivity {
         //释放资源
         mPlayer.releasePlayer();
     }
+
+    class ItemTask extends AsyncTask<Void, String, String> {
+        String sql;
+
+        ItemTask(String sql) {
+            this.sql = sql;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            Map<String, String> map = new HashMap<>();
+            map.put("FSql", sql);
+            map.put("FTable", "t_icitem");
+            return SoapUtil.requestWebService(Consts.JA_select, map);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Document doc = DocumentHelper.parseText(s);
+                Element ele = doc.getRootElement();
+                Iterator iter = ele.elementIterator("Cust");
+                HashMap<String, String> map = new HashMap<>();
+                while (iter.hasNext()) {
+                    Element recordEle = (Element) iter.next();
+                    map.put("token", recordEle.elementTextTrim("token"));//物料内码(提交订单用)
+                }
+                //填充数据到页面
+                String tokenid = map.get("token");
+                if (tokenid.startsWith("at.")) {
+                    token = tokenid;
+                }
+                //初始化摄像头
+                initCamera();
+            } catch (Exception e) {
+                e.printStackTrace();
+                ToastUtils.showToast(JiankongActivity.this, "未查获取到token");
+                finish();
+            }
+            ProgressDialogUtil.hideDialog();
+        }
+    }
+    /*
+    *
+    * */
 }
--- app/src/main/java/com/example/win7/ytdemo/activity/LoginActivity.java
+++ app/src/main/java/com/example/win7/ytdemo/activity/LoginActivity.java
@@ -8,12 +8,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.win7.ytdemo.R;
 import com.example.win7.ytdemo.YApplication;
 import com.example.win7.ytdemo.util.Consts;
+import com.example.win7.ytdemo.util.ThreadUtils;
 import com.example.win7.ytdemo.util.Utils;
 import com.example.win7.ytdemo.view.CustomProgress;
 import com.hyphenate.EMCallBack;
@@ -25,11 +27,12 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 
 public class LoginActivity extends BaseActivity {
-    EditText et_username,et_password;
-    Button btn_login;
-    CustomProgress dialog;
-    LinearLayout ll_login;
+    EditText et_username, et_password;
+    Button            btn_login;
+    CustomProgress    dialog;
+    LinearLayout      ll_login;
     SharedPreferences sp;
+    private ImageView mView_logout;
 
 
     @Override
@@ -40,44 +43,88 @@ public class LoginActivity extends BaseActivity {
         setListeners();
     }
 
-    protected void setViews(){
-        sp = getSharedPreferences("token",MODE_PRIVATE);
-        ll_login = (LinearLayout)findViewById(R.id.ll_login);
-        et_username = (EditText)findViewById(R.id.et_username);
-        et_password = (EditText)findViewById(R.id.et_password);
+    protected void setViews() {
+        sp = getSharedPreferences("token", MODE_PRIVATE);
+        mView_logout = findViewById(R.id.view_logout);
+        ll_login = (LinearLayout) findViewById(R.id.ll_login);
+        et_username = (EditText) findViewById(R.id.et_username);
+        et_password = (EditText) findViewById(R.id.et_password);
         et_password.requestFocus();
-        btn_login = (Button)findViewById(R.id.btn_login);
-        et_username.setText(sp.getString("fname",""));
+        btn_login = (Button) findViewById(R.id.btn_login);
+        String fname = sp.getString("fname", "");
+        String fgroup = sp.getString("fgroup", "");
+        et_username.setText(fname);
         Utils.autoScrollView(ll_login, btn_login);//弹出软键盘时滚动视图
+        if (null != sp && !"".equals(fname) && null != fgroup && !"".equals(fgroup)) {
+            YApplication.fname = fname;
+            YApplication.fgroup = fgroup;
+            //登录环信聊天服务器
+            EMClient.getInstance().login(fname, "123", new EMCallBack() {//回调
+                @Override
+                public void onSuccess() {
+                    EMClient.getInstance().groupManager().loadAllGroups();
+                    EMClient.getInstance().chatManager().loadAllConversations();
+                    Log.d("main", "登录聊天服务器成功！");
+                }
+
+                @Override
+                public void onProgress(int progress, String status) {
+
+                }
+
+                @Override
+                public void onError(int code, String message) {
+                    Log.d("main", "登录聊天服务器失败！");
+                }
+            });
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
-    protected void setListeners(){
+    protected void setListeners() {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(et_username.getText().toString().equals("")||et_password.getText().toString().equals("")){
-                    Toast.makeText(LoginActivity.this,"请填写完整用户名和密码",Toast.LENGTH_SHORT).show();
-                }else {
+                if (et_username.getText().toString().equals("") || et_password.getText().toString().equals("")) {
+                    Toast.makeText(LoginActivity.this, "请填写完整用户名和密码", Toast.LENGTH_SHORT).show();
+                } else {
                     //登录K3用户
                     new LTask(et_username.getText().toString(), et_password.getText().toString()).execute();
+                }
+            }
+        });
+        mView_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String name = et_username.getText().toString();
+                if (null != name && !"".equals(name)) {
+                    //退出当前账号
+                    ThreadUtils.runOnSubThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Exit(name);
+                        }
+                    });
                 }
             }
         });
     }
 
 
-    class LTask extends AsyncTask<Void,String,String> {
+    class LTask extends AsyncTask<Void, String, String> {
         String name;
         String pass;
 
-        LTask(String name,String pass){
+        LTask(String name, String pass) {
             this.name = name;
             this.pass = pass;
         }
 
         @Override
         protected void onPreExecute() {
-            dialog = CustomProgress.show(LoginActivity.this,"登录中...",true,null);
+            dialog = CustomProgress.show(LoginActivity.this, "登录中...", true, null);
             super.onPreExecute();
         }
 
@@ -122,17 +169,17 @@ public class LoginActivity extends BaseActivity {
 
             // 获取返回的结果
             if (null != object) {
-                Log.i("返回结果", object.getProperty(0).toString()+"=========================");
+                Log.i("返回结果", object.getProperty(0).toString() + "=========================");
                 String result = object.getProperty(0).toString();
-                if (result.substring(0,1).equals("0")) {
+                if (result.substring(0, 1).equals("0")) {
                     YApplication.fname = name;
-                    YApplication.fgroup = result.replace("0","");
+                    YApplication.fgroup = result.replace("0", "");
                     return "0";
-                } else if(result.equals("1")){
+                } else if (result.equals("1")) {
                     return "1";
-                }else if(result.equals("2")){
+                } else if (result.equals("2")) {
                     return "2";
-                }else if (result.equals("3")){
+                } else if (result.equals("3")) {
                     return "3";
                 }
             }
@@ -142,7 +189,7 @@ public class LoginActivity extends BaseActivity {
         @Override
         protected void onPostExecute(String s) {
             dialog.dismiss();
-            if(s.equals("0")) {
+            if (s.equals("0")) {
                 //登录环信聊天服务器
                 EMClient.getInstance().login(et_username.getText().toString(), "123", new EMCallBack() {//回调
                     @Override
@@ -164,23 +211,58 @@ public class LoginActivity extends BaseActivity {
                 });
                 //保存用户名，用户组
                 SharedPreferences.Editor editor = sp.edit();
-                editor.putString("fname",name);
-                editor.putString("fgroup",YApplication.fgroup);
-                editor.putString("status","1");
+                editor.putString("fname", name);
+                editor.putString("fgroup", YApplication.fgroup);
+                editor.putString("status", "1");
                 editor.commit();
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
                 finish();
-            }else if(s.equals("1")) {
-                Toast.makeText(LoginActivity.this,"密码错误",Toast.LENGTH_SHORT).show();
-            }else if(s.equals("2")) {
-                Toast.makeText(LoginActivity.this,"用户名不存在",Toast.LENGTH_SHORT).show();
-            }else if(s.equals("3")) {
-                Toast.makeText(LoginActivity.this,"当前账号已在线",Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(LoginActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+            } else if (s.equals("1")) {
+                Toast.makeText(LoginActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
+            } else if (s.equals("2")) {
+                Toast.makeText(LoginActivity.this, "用户名不存在", Toast.LENGTH_SHORT).show();
+            } else if (s.equals("3")) {
+                Toast.makeText(LoginActivity.this, "当前账号已在线", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(LoginActivity.this, "登录失败", Toast.LENGTH_SHORT).show();
             }
             super.onPostExecute(s);
+        }
+    }
+
+    protected static void Exit(String fname) {
+        // 命名空间
+        String nameSpace = "http://tempuri.org/";
+        // 调用的方法名称
+        String methodName = "Z_Exit";
+        // EndPoint
+        String endPoint = Consts.ENDPOINT;
+        // SOAP Action
+        String soapAction = "http://tempuri.org/Z_Exit";
+
+        // 指定WebService的命名空间和调用的方法名
+        SoapObject rpc = new SoapObject(nameSpace, methodName);
+
+        // 设置需调用WebService接口需要传入的两个参数mobileCode、userId
+        rpc.addProperty("UserName", fname);
+
+        // 生成调用WebService方法的SOAP请求信息,并指定SOAP的版本
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER10);
+
+        envelope.bodyOut = rpc;
+        // 设置是否调用的是dotNet开发的WebService
+        envelope.dotNet = true;
+        // 等价于envelope.bodyOut = rpc;
+        envelope.setOutputSoapObject(rpc);
+
+        HttpTransportSE transport = new HttpTransportSE(endPoint);
+        try {
+            // 调用WebService
+            transport.call(soapAction, envelope);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("LoginActivity", e.toString() + "==================================");
         }
     }
 }
